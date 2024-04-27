c7f984d10304e564d5c3ffa10ace5de7980cb84f
Barsch2006 <christian0511@gmx.de>
2024-04-27 11:37:41 +0200

    logger, cors

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -2,5 +2,9 @@ import { config } from "dotenv";
 import { z } from "zod";
 config();
 
-const envSchema = z.object({});
+const envSchema = z.object({
+    LOG_DIR: z.string(),
+    PORT: z.string(),
+    FRONTEND_URL: z.string(),
+});
 export default envSchema.parse(process.env);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,27 @@
-(async () => {})();
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "https";
+
+import env from "./env";
+import getLogger from "./logger";
+
+const app = express();
+const server = http.createServer(app); // make https on production
+const mainLogger = getLogger("main");
+
+(async () => {
+    app.use(
+        cors({
+            origin: env.FRONTEND_URL,
+        }),
+    );
+    app.use(express.json());
+    app.use(cookieParser());
+
+    // TODO: Add http endpoint handlers here
+
+    server.listen(env.PORT, () => {
+        mainLogger("INFO", `Server is running on port ${env.PORT}`);
+    });
+})();
