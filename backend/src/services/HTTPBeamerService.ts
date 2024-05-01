04c6a9ad41e975b4a24587f50fd356dadf2b21c2
heeecker <89743412+filip326@users.noreply.github.com>
2024-05-01 15:59:50 +0200

    Add sendRs232Command method to HTTPBeamerService

--- backend/src/services/HTTPBeamerService.ts
+++ backend/src/services/HTTPBeamerService.ts
@@ -1,4 +1,5 @@
 import env from "../env";
+import { HttpV1_0_Request } from "../utils/HttpV1_0";
 import getLogger from "../utils/logger";
 
 class HTTPBeamerService {
@@ -12,6 +13,16 @@ class HTTPBeamerService {
     private constructor(ip: string) {
         this.url = `http://${ip}`;
         this.log("INFO", "HTTPBeamerService initialized");
+    }
+
+    private sendRs232Command(command: string) {
+        const request = new HttpV1_0_Request();
+        request.method = "POST";
+        request.host = this.url;
+        request.path = "/cgi-bin/MMX32_Keyvalue.cgi";
+        request.body = `{CMD=>${command}\r\n`;
+
+        return;
     }
 
     private log = getLogger("HTTPBeamerService");
