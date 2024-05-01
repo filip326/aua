c645d8a278cb7fbac5ea379f313116ea72c2043b
heeecker <89743412+filip326@users.noreply.github.com>
2024-05-01 15:42:25 +0200

    Refactor ArtNetService to use private method for building artnet package instead of an util

--- backend/src/services/ArtNetService.ts
+++ backend/src/services/ArtNetService.ts
@@ -1,7 +1,6 @@
 import getLogger from "../utils/logger";
 import dgram from "dgram";
 import env from "../env";
-import buildArtNetPackage from "../utils/artnet";
 
 class ArtNetService {
     private static _instance: ArtNetService | null = null;
@@ -42,6 +41,36 @@ class ArtNetService {
         this.log("INFO", "ArtNetService initialized");
     }
 
+    /**
+     * Builds an artnet package with all the required headers and data
+     * @param universe the universe of the artnet package
+     * @param data all data to be sent/ packed into the artnet package
+     * @returns the artnet package as an array which just needs to be converted to a buffer before sending it
+     */
+    private buildArtNetPackage(universe: number, data: Array<number>): Array<number> {
+        let lenght = data.length;
+
+        if (lenght > 512) {
+            // DMX512 = 512 channels = 512 bytes
+            lenght = 512;
+        }
+
+        if (lenght % 2) {
+            lenght += 1;
+        }
+
+        const hUni = (universe >> 8) & 0xff;
+        const lUni = universe & 0xff;
+        const hLen = (lenght >> 8) & 0xff;
+        const lLen = lenght & 0xff;
+
+        // Protocol Name, Version, Sequence, Universe, Data Length
+        const artnetHeader = [65, 114, 116, 45, 78, 101, 116, 0, 0, 80, 0, 14, 0, 0, lUni, hUni, hLen, lLen];
+        const artnetPackage = artnetHeader.concat(data.slice(0, hLen * 256 + lLen));
+
+        return artnetPackage;
+    }
+
     public sendData(
         universe: number,
         data: Uint8ClampedArray,
@@ -57,7 +86,7 @@ class ArtNetService {
             return;
         }
 
-        const artnetPackage = buildArtNetPackage(universe, Array.from(data));
+        const artnetPackage = this.buildArtNetPackage(universe, Array.from(data));
         const bufferedPackage = Buffer.from(artnetPackage);
 
         this.socket.send(bufferedPackage, 0, bufferedPackage.length, 6454, ip, cb);
