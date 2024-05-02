52e95191394c515f12e630f5c03b7c0181eccf3f
Barsch2006 <christian0511@gmx.de>
2024-05-02 18:49:47 +0200

    major improvements on pi beamer design

--- frontend/src/components/SquareIconBtn.vue
+++ frontend/src/components/SquareIconBtn.vue
@@ -7,10 +7,6 @@ export default {
             type: String,
             default: "surface",
         },
-        width: {
-            type: String,
-            default: "130px",
-        },
         iconSize: {
             type: String,
             default: "40px",
@@ -24,13 +20,10 @@ export default {
 </script>
 
 <template>
-    <VBtn
-        variant="flat"
-        :color="color"
-        :style="`width: ${width}; aspect-ratio: 1/1; height: auto; position: relative;`"
-    >
+    <VBtn variant="flat" :color="color" style="position: relative">
         <VIcon :size="iconSize">{{ icon }}</VIcon>
-        <span class="square_icon_btn__text"
+        <span
+            class="square_icon_btn__text"
             :style="`position: absolute; bottom: 10px; left: 0; width: 100%; text-align: center; font-size: ${textSize}`"
         >
             {{ text }}
--- frontend/src/pages/pi/Index.vue
+++ frontend/src/pages/pi/Index.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import SquareIconBtn from "../../components/SquareIconBtn.vue";
+import "../../styles/pi/beamer.css";
 
 export default {
     components: {
@@ -21,27 +22,27 @@ export default {
 
     <VWindow v-model="tab">
         <VWindowItem value="beamer">
-            <div class="content__wrapper">
-                <VCard color="darker" variant="flat" width="calc(50% - 10px)" style="aspect-ratio: 370/252">
-                    <VCardTitle> Beamer An/ Aus </VCardTitle>
-                    <VCardItem class="centered_content">
-                        <SquareIconBtn width="calc(50% - 10px)" icon="mdi-projector" text="Beamer an" />
-                        <SquareIconBtn width="calc(50% - 10px)" icon="mdi-projector-off" text="Beamer aus" />
+            <div class="pi__content_wrapper">
+                <VCard class="centered_content_card" color="darker" variant="flat">
+                    <VCardTitle> An/ Aus</VCardTitle>
+                    <VCardItem>
+                        <SquareIconBtn class="square_icon_btn" icon="mdi-projector" text="Beamer an" />
+                        <SquareIconBtn class="square_icon_btn" icon="mdi-projector-off" text="Beamer aus" />
                     </VCardItem>
                 </VCard>
 
-                <VCard color="darker" variant="flat" width="calc(50% - 10px)" style="aspect-ratio: 370/252">
+                <VCard class="centered_content_card" color="darker" variant="flat">
                     <VCardTitle> Bild-Format </VCardTitle>
-                    <VCardItem class="centered_content">
-                        <SquareIconBtn width="calc(50% - 10px)" icon="mdi-monitor" text="16 / 9" textSize="14px" />
-                        <SquareIconBtn width="calc(50% - 10px)" icon="mdi-monitor-small" text="4 / 3" textSize="14px" />
+                    <VCardItem>
+                        <SquareIconBtn class="square_icon_btn" icon="mdi-monitor" text="16 / 9" textSize="14px" />
+                        <SquareIconBtn class="square_icon_btn" icon="mdi-monitor-small" text="4 / 3" textSize="14px" />
                     </VCardItem>
                 </VCard>
 
-                <VCard color="darker" variant="flat" width="100%">
+                <VCard class="video_selection_card" color="darker" variant="flat">
                     <VCardTitle> Bildquelle </VCardTitle>
-                    <VCardItem id="videosource_selection_button_group">
-                        <VBtn width="200px" height="42px" v-for="src in video_srces" :key="src">
+                    <VCardItem>
+                        <VBtn variant="flat" width="200px" height="42px" v-for="src in video_srces" :key="src">
                             {{ src }}
                         </VBtn>
                     </VCardItem>
@@ -50,22 +51,3 @@ export default {
         </VWindowItem>
     </VWindow>
 </template>
-
-<style>
-@import url(../../styles/pi.__global.css);
-</style>
-
-<style scoped>
-.content__wrapper {
-    height: calc(100svh - 48px);
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    padding: 20px;
-    gap: 20px;
-}
-
-.content__wrapper > * > .v-card-title {
-    text-align: center;
-}
-</style>
