cb99ea2901137a0481c3ea412284ad68bda8e603
Barsch2006 <christian0511@gmx.de>
2024-05-04 14:46:57 +0200

    some changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import http from "https";
+import http from "http";
 
 import env from "./env";
 import getLogger from "./utils/logger";
