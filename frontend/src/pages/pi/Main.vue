59dee19bfc0537c38805ba5e30d2431e83e1c10d
Barsch2006 <christian0511@gmx.de>
2024-05-05 14:54:52 +0200

    x32 layout design

--- frontend/src/pages/pi/Main.vue
+++ frontend/src/pages/pi/Main.vue
@@ -1,9 +1,11 @@
 <script lang="ts">
 import SubviewBeamer from "./_Beamer.vue";
+import SubviewX32 from "./_X32.vue";
 
 export default {
     components: {
         SubviewBeamer,
+        SubviewX32
     },
     data() {
         return {
@@ -16,11 +18,15 @@ export default {
 <template>
     <VTabs bg-color="secondary" v-model="tab" fixed-tabs>
         <VTab value="beamer">Beamer</VTab>
+        <VTab value="sound">Ton</VTab>
     </VTabs>
 
     <VWindow v-model="tab">
         <VWindowItem value="beamer">
             <SubviewBeamer />
+        </VWindowItem>
+        <VWindowItem value="sound">
+            <SubviewX32 />
         </VWindowItem>
     </VWindow>
 </template>
---
+++ frontend/src/pages/pi/_X32.vue
@@ -0,0 +1,115 @@
+<script lang="ts">
+import SquareIconBtn from "../../components/SquareIconBtn.vue";
+import PiLayout from "../../layouts/PiLayout.vue";
+
+export default {
+    components: {
+        SquareIconBtn,
+        PiLayout,
+    },
+    data() {
+        return {
+            scenes: ["Default"],
+        };
+    },
+    methods: {},
+};
+</script>
+
+<template>
+    <PiLayout>
+        <template #vcard_left>
+            <VCardTitle> Mikrofone </VCardTitle>
+            <VCardItem>
+                <div class="channel">
+                    <h3>M1</h3>
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-off" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-plus" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-minus" />
+                </div>
+                <div class="channel">
+                    <h3>M2</h3>
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-off" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-plus" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-minus" />
+                </div>
+                <div class="channel">
+                    <h3>M3</h3>
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-off" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-plus" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-minus" />
+                </div>
+                <div class="channel">
+                    <h3>H1</h3>
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-off" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-plus" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-minus" />
+                </div>
+            </VCardItem>
+        </template>
+
+        <template #vcard_right>
+            <VCardTitle> Multimedia </VCardTitle>
+            <VCardItem>
+                <div class="channel has-icon">
+                    <VIcon style="margin-bottom: 2px">mdi-bluetooth</VIcon>
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-off" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-plus" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-minus" />
+                </div>
+                <div class="channel has-icon">
+                    <VIcon style="margin-bottom: 2px">mdi-projector</VIcon>
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-off" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-plus" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-minus" />
+                </div>
+                <div class="channel has-icon">
+                    <VIcon style="margin-bottom: 2px">mdi-audio-input-stereo-minijack</VIcon>
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-off" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-plus" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-minus" />
+                </div>
+                <div class="channel has-icon">
+                    <VIcon style="margin-bottom: 2px">mdi-volume-vibrate</VIcon>
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-off" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-plus" />
+                    <VBtn rounded="0" width="64px" height="42px" variant="flat" icon="mdi-volume-minus" />
+                </div>
+            </VCardItem>
+        </template>
+
+        <template #vcard_bottom>
+            <VCardTitle> Scenen </VCardTitle>
+            <VCardItem>
+                <VBtn variant="flat" width="200px" height="42px" v-for="scene in scenes" :key="scene">{{ scene }}</VBtn>
+            </VCardItem>
+        </template>
+    </PiLayout>
+</template>
+
+<style scoped>
+.channel {
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    height: 100%;
+    gap: 6px;
+    flex-direction: column;
+    padding-top: 1px;
+    flex: 0 1 calc(50% - 12px);
+}
+
+.channel.has-icon {
+    padding-top: 3px;
+}
+
+.channel h3 {
+    font-weight: 500;
+}
+
+@media screen and (max-width: 700px) {
+    .channel:not(:nth-child(1)):not(:nth-child(2)) {
+        margin-top: 16px;
+    }
+}
+</style>
