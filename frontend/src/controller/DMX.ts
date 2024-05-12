4b40818c8d0415cf0a9697801eff2889075c0b9b
Barsch2006 <christian0511@gmx.de>
2024-05-12 21:28:20 +0200

    fixes

--- frontend/src/controller/DMX.ts
+++ frontend/src/controller/DMX.ts
@@ -2,62 +2,62 @@ import useFetch from "./_useFetch";
 
 export default class DMX {
     static sceneAnlageAn() {
-        return useFetch("/dmx/scene/anlage-an");
+        return useFetch("/dmx/scene/anlage-an", "POST");
     }
 
     static sceneAnlageAus() {
-        return useFetch("/dmx/scene/anlage-aus");
+        return useFetch("/dmx/scene/anlage-aus", "POST");
     }
 
     static clearScenes() {
-        return useFetch("/dmx/scene/clear");
+        return useFetch("/dmx/scene/clear", "POST");
     }
 
     static stageWW() {
-        return useFetch("/dmx/scene/stage-ww");
+        return useFetch("/dmx/scene/stage-ww", "POST");
     }
 
     static stageCW() {
-        return useFetch("/dmx/scene/stage-cw");
+        return useFetch("/dmx/scene/stage-cw", "POST");
     }
 
     static stageOff() {
-        return useFetch("/dmx/scene/stage-off");
+        return useFetch("/dmx/scene/stage-off", "POST");
     }
 
     static stageV1() {
-        return useFetch("/dmx/scene/stage-v1");
+        return useFetch("/dmx/scene/stage-v1", "POST");
     }
 
     static stageV2() {
-        return useFetch("/dmx/scene/stage-v2");
+        return useFetch("/dmx/scene/stage-v2", "POST");
     }
 
     static stageV3() {
-        return useFetch("/dmx/scene/stage-v3");
+        return useFetch("/dmx/scene/stage-v3", "POST");
     }
 
     static spotLeft() {
-        return useFetch("/dmx/scene/spot-left");
+        return useFetch("/dmx/scene/spot-left", "POST");
     }
 
     static spotMid() {
-        return useFetch("/dmx/scene/spot-mid");
+        return useFetch("/dmx/scene/spot-mid", "POST");
     }
 
     static spotOff() {
-        return useFetch("/dmx/scene/spot-off");
+        return useFetch("/dmx/scene/spot-off", "POST");
     }
 
     static accentV1() {
-        return useFetch("/dmx/scene/accent-v1");
+        return useFetch("/dmx/scene/accent-v1", "POST");
     }
 
     static accentV2() {
-        return useFetch("/dmx/scene/accent-v2");
+        return useFetch("/dmx/scene/accent-v2", "POST");
     }
 
     static discoParty() {
-        return useFetch("/dmx/scene/disco");
+        return useFetch("/dmx/scene/disco", "POST");
     }
 }
